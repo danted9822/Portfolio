dda120ce094d0ad98fdd8acb34e9d1419d997005
danted9822 <halaszpeter9814@gmail.com>
2022-10-15 13:35:17 +0200

    Add new projects , update the current design on mobile view and rewrite the existing text.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
 	const { t } = useTranslation(["contact"]);
 
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
+    <div name='contact' className='w-full h-auto bg-[#0a192f] flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/959c1107-e1e0-445b-8f46-1b202fbf77af" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>{t("contact")}</p>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 	const { t } = useTranslation(["home"]);
   
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full md:h-screen h-auto bg-[#0a192f] pt-20'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p  className='text-pink-600'>{t("greeting")}</p>
@@ -17,11 +17,11 @@ const Home = () => {
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
         {t("junior")}
         </h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+        <p className='text-[#8892b0] py-4 mb-10 max-w-[700px]'>
         {t("about")}
         </p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+          <button className='text-white group border-2 px-6 py-3  mb-20 flex items-center hover:bg-pink-600 hover:border-pink-600'>
             <Link to='work' smooth={true} duration={500}>
             {t("work")}
             </Link>
