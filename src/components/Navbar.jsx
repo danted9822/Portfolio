b25cb6373fd784d71ae577229b59c47c73d174ca
Halász Péter <72155774+danted9822@users.noreply.github.com>
2025-03-22 13:25:18 +0100

    Modify language texts, update packages and add new skills

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
 
   useEffect(() => {
     if (localStorage.getItem("i18nextLng")?.length > 2) {
-      i18next.changeLanguage("en");
+      i18next.changeLanguage("hu");
     }
   }, []);
 
@@ -32,7 +32,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-        <img src={Logo} alt='Logo Image' style={{ width: '150px' }} />
+        <img src={Logo} alt='Logo' style={{ width: '150px' }} />
       </div>
 
       {/* menu */}
